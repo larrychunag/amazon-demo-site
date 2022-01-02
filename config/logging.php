6b9e1ad0b903acce5a2179cdf02906fc6a000704
larry <larry@larrynoMacBook-Pro.local>
2022-01-02 22:31:27 +0900

    feat: add vue auth and ci

--- config/logging.php
+++ config/logging.php
@@ -5,7 +5,6 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Log Channel
@@ -108,5 +107,4 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
     ],
-
 ];
