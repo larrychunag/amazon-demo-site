29b42cdc492ee8e05020ad8af2c4f114982b8c78
larry <larry@larrynoMacBook-Pro.local>
2022-01-03 18:38:22 +0900

    feat: add route

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -4,11 +4,15 @@ use Illuminate\Routing\Router;
 
 Admin::routes();
 
-Route::group([
-    'prefix' => config('admin.route.prefix'),
-    'namespace' => config('admin.route.namespace'),
-    'middleware' => config('admin.route.middleware'),
-    'as' => config('admin.route.prefix').'.',
-], function (Router $router) {
-    $router->get('/', 'HomeController@index')->name('home');
-});
+Route::group(
+    [
+        'prefix' => config('admin.route.prefix'),
+        'namespace' => config('admin.route.namespace'),
+        'middleware' => config('admin.route.middleware'),
+        'as' => config('admin.route.prefix').'.',
+    ],
+    function (Router $router) {
+        $router->get('/', 'HomeController@index')->name('home');
+        $router->resource('/users', UserController::class);
+    }
+);
